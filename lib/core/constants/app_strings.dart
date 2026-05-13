c31b3f8b3075a5e1b71d525bfd0e3d2e5103a14e
Turain Software <turain@Turains-Mac-mini.local>
2026-05-13 12:57:20 +0530

    update token

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -4,5 +4,5 @@ class AppStrings {
   static const profile = "Profile";
   static const guestUser = "Guest User";
   static const searchBycate="Shop by Category";
-  static const token="5|eRPODo7t7bQCIbEYQ1kZi7CIcQa4lL9lak4VR8HS616df259";
+  static const token="2|IcBA3X5EoOtULY0JDaCSBJn10taNPQExMr2WDPjJf3854a0a";
 }
